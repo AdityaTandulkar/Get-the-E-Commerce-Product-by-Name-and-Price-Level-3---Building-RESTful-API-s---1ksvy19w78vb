532e35312a14073a43a4075183c16c6650fb2536
aditya <adityatandulkar20@gmail.com>
2023-04-24 22:09:52 +0530

    Intital commit

--- src/app.js
+++ src/app.js
@@ -11,4 +11,28 @@ app.use(express.json());
 // Write GET endpoint for sending product to the client here
 // Endpoint - /api/v1/products/:name/:price
 
+app.get("/api/v1/products/:name/:price", (req, res)=>{
+    const {name, price} = req.params;
+
+    const filtered = products.find((item) => item.name === name && item.price == price);
+
+    if(filtered){
+        res.send({
+            status : "success",
+            message : "Product fetched successfully",
+            data : {
+                filtered
+            }
+        })
+    }
+    else{
+        res.status(404).send({
+            status : "failed",
+            message : "Product not found!"
+        })
+    }
+})
+
+app.listen(8080)
+
 module.exports = app;
